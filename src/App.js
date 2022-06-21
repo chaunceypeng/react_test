182cd7c3c99ce4334c63e05cf707920be6953417
chaunceypeng <v-cpeng@microsoft.com>
2022-06-21 10:52:52 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [items, setItems] = useState([]);
+
+
+  // This function mocks a simple synchronous API to fetch the label list by keyword.
+  // Example:
+  //  const val = getLabels('C');
+  //  console.log(val);
+  function getLabels(keyword) {
+    const allLabels = ['NextActions', 'Someday_Actions', 'Costco', 'Alexa'];
+    const result = allLabels
+      .filter(function (x) {
+        return x.toLowerCase().indexOf(keyword.toLowerCase()) > -1;
+      });
+    return result;
+  }
+
+  // This function mocks the asynchronous API to fetch the label list by keyword.
+  // Example:
+  //  getLabelsAsync('C').then(function(val) {
+  //     console.log(val);
+  //  })
+  function getLabelsAsync(keyword) {
+    const result = getLabels(keyword);
+    const delay = Math.random() * 800 + 200; // delay 200~1000ms
+    return new Promise(function (resolve, reject) {
+      setTimeout(resolve, delay, result);
+    });
+  }
+
+  const handleChange = (e) => {
+    let value = e.target.value
+    console.log(value);
+    var start = value.indexOf('@');
+    if (start > -1) {
+      var words = value.substring(start + 1, value.length);
+      getLabelsAsync(words).then(function (val) {
+        setItems(val);
+      })
+    }
+    else {
+      setItems([])
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='dropdown'>
+          <div>
+            <input className='input' type={"text"} onChange={(e) => handleChange(e)} />
+          </div>
+          <div className='content'>           
+              {items.map((ele, i) => {
+                return  <option className='item' key={i}>{ele}</option>
+              })}            
+          </div>
+        </div>
       </header>
     </div>
   );
 }
 
-export default App;
+
+
